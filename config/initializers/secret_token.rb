1df70e93f622927421ec96679347bd470a1ad960
Steven Lotito <slotito@zappos.com>
2013-08-21 15:14:57 -0700

    Finally got app at least to the welcome page

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StacksOnStacks::Application.config.secret_key_base = '843cb1a767a45cbb0a3207600b3ba5fb06f36aa9abf117ea42121fa3c6674d01fb0bc4562afd7d75f3990a4b1a11ce2b4436409d883b74cd4619d223ca0008be'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+StacksOnStacks::Application.config.session_store :cookie_store, key: '_stacks_on_stacks_session'
