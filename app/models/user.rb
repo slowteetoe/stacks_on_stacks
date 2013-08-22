cd19c3c9f6140d54f24ad99364a0e369e19ca3f1
Fred Guest <fguest@zappos.com>
2013-08-21 17:07:28 -0700

    change devise key and views to username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,17 @@
 class User
   include Mongoid::Document
+
+  validates_presence_of :username
+  validates_uniqueness_of :username
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable
 
   ## Database authenticatable
   field :email,              :type => String, :default => ""
+  field :username,           :type => String, :default => ""
   field :encrypted_password, :type => String, :default => ""
 
   ## Recoverable
