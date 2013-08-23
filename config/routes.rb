489cab1413db4a737b9d6ad776e5d4dd21026bef
Fred Guest <fguest@zappos.com>
2013-08-23 11:17:10 -0700

    add routes and controller for user show and index

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ StacksOnStacks::Application.routes.draw do
   resources :questions
 
   devise_for :users
-  get "search", to: "search#index"
-  root :to => "pages#home"
+  get '/users' => 'users#index'
+  get '/users/:id' => 'users#show'
+  get '/search' => 'search#index'
+  root :to => 'pages#home'
 
 end
