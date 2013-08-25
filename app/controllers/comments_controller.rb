fd907692f747d6983367a2ae8eee350b8107f8c2
Fred Guest <fguest@zappos.com>
2013-08-25 10:32:49 -0700

    add front end for voting

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,10 @@
 class CommentsController < ApplicationController
 
-  def create
-  	if user_signed_in?
-  		question = Question.find(params[:q_id])
+	def create
+		if user_signed_in?
+			question = Question.find(params[:q_id])
 
-	  	if comment_on_question?	
+			if comment_on_question?	
 				question.comments << build_comment
 				question.save!
 				redirect_to question, notice: "Comment submitted!" 
@@ -13,30 +13,28 @@ class CommentsController < ApplicationController
 				answer.comments << build_comment
 				answer.save!
 				redirect_to question, notice: "Comment submitted!" 
-			else
-				redirect_to '/', notice: "Not so much." 
 			end
 		else
 			redirect_to '/users/sign_in', alert: "You need to sign in or sign up before continuing."
 		end
-  end
+	end
 
-  private
+	private
 
-  def comment_params
-    params.require(:comment).permit(:body, :q_id, :a_id)
-  end
+	def comment_params
+		params.require(:comment).permit(:body, :q_id, :a_id)
+	end
 
-  def build_comment
-  	Comment.new(body: params[:body], author: current_user.username)
-  end
+	def build_comment
+		Comment.new(body: params[:body], author: current_user.username)
+	end
 
-  def comment_on_question?
-  	!!params[:q_id] && !params[:a_id]
-  end
+	def comment_on_question?
+		!params[:a_id]
+	end
 
-  def comment_on_answer?
-  	!!params[:a_id]
-  end
+	def comment_on_answer?
+		!!params[:a_id]
+	end
 
 end
