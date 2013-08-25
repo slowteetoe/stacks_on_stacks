eb217d2968014fe57b7eb9ec795f58f32c2597ad
Steven Lotito <slotito@zappos.com>
2013-08-25 10:22:57 -0700

    Add rudimentary support for [code] tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  OPEN_CODE_TAG = '<code class="code"><pre>'
+  CLOSE_CODE_TAG = '</pre></code>'
+
+  def fix_code_tags(s)
+    s.gsub(/\[\/code\]/, CLOSE_CODE_TAG).gsub(/\[code\]/,OPEN_CODE_TAG).html_safe
+  end
 end
