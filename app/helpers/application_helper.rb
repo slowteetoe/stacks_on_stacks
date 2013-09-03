11621776f2a611e67eb87388d83c8d3dc219e944
Fred Guest <fguest@zappos.com>
2013-09-03 16:19:40 -0700

    add user_link helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,9 +6,17 @@ module ApplicationHelper
     html_escape(s).gsub(/\[\/code\]/, CLOSE_CODE_TAG).gsub(/\[code\]/,OPEN_CODE_TAG).html_safe
   end
 
-  def author_link(username, profile)
-  	name = !!profile.display_name ? profile.display_name : username
-  	return link_to name, "/users/#{profile.user_id}"
+	def user_link(username, profile)
+		if !!profile.display_name
+			return link_to "#{profile.display_name} (#{username})", "/users/#{profile.user_id}"
+		else
+			return link_to username, "/users/#{profile.user_id}"
+		end
+	end
+
+	def author_link(username, profile)
+		name = !!profile.display_name ? profile.display_name : username
+		return link_to name, "/users/#{profile.user_id}"
 	end
 
 	def profile_name(user)
