4fe9527b5619823db4b86355c16c727979415753
Fred Guest <fguest@zappos.com>
2013-09-27 14:35:21 -0700

    add custom auth initializer

---
+++ config/initializers/kerberos.rb
@@ -0,0 +1,33 @@
+require 'devise/strategies/authenticatable'
+ 
+module Devise
+  module Strategies
+    class KerberosAuthenticatable < Authenticatable
+
+	    def valid?
+	      username && password
+	    end
+ 
+	    def authenticate!    
+	      if username == 'foo' && password == 'bar'
+	        user = User.where(username: username.downcase).first_or_create
+	        success! user
+	      else
+	        fail! "Sorry, your username or password is incorrect"
+	      end
+	    end
+
+      private
+
+			def username
+			  (params[:user] || {})[:username]
+			end
+
+			def password
+			  (params[:user] || {})[:password]
+			end
+    end 
+  end 
+end
+
+Warden::Strategies.add(:kerberos_authenticatable, Devise::Strategies::KerberosAuthenticatable)
